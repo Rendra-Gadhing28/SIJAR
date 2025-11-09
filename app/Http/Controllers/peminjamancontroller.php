c662602f09eeab7ae5ec1b60aa2d8afa549feae4
Arka Aceananda <aceanandptr12@gmail.com>
2025-11-09 16:42:42 +0700

    bobob

--- app/Http/Controllers/peminjamancontroller.php
+++ app/Http/Controllers/peminjamancontroller.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use App\Models\item;
 use App\Models\Peminjaman;
 
 class PeminjamanController extends Controller
 {
     public function index(){
-        return view('user.pinjam');
+        $peminjaman = Peminjaman::where("user_id", Auth::id())
+            ->with(["barang", "jam"])
+            ->latest()
+            ->paginate(10);
+        return view('user.pinjam', compact("peminjaman"));
     }
 
     public function show($id){
